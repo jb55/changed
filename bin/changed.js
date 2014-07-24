14121552e4a1045630c2c2bf720834a082d2dbd7
William Casarin <bill@casarin.me>
2014-07-24 13:11:37 -0700

    store cli option

--- bin/changed.js
+++ bin/changed.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
 
 var changed = require('../');
+var argv = require('minimist')(process.argv.slice(2));
 
 function usage() {
-  console.error("usage: changed file1.txt file2.txt");
+  console.error("usage: changed [-s /tmp/store] file1.txt file2.txt");
   return process.exit(1);
 }
 
-if (process.argv.length < 3)
+if (argv._.length === 0)
   return usage();
 
-var files = process.argv.slice(2);
-changed(files, function(err, files){
+var store = argv.s || "/tmp/changed.store";
+
+changed(argv._, { store: store }, function(err, files){
   files.forEach(function(file){
     console.log(file);
   });
